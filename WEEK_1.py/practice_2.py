1b7880138152a5cb36d66d5143749ad3cca665a9
tadeyoyin1 <tadeyoyin1@gmail.com>
2025-03-22 13:48:24 +0100

    MESSAGE

---
+++ WEEK_1.py/practice_2.py
@@ -0,0 +1,12 @@
+#PYTHON PROGRAM TO PRINT STRINGS AND TYPE
+str1 = "Hi my name is Timilehin. I am  string "
+str2 = "Hi my name is Oge. I am also a string"
+
+#displaying string str1 and its type
+print(str1)
+print(type(str1))
+
+#displaying string str2 and its type
+print(str2)
+print(type(str2))
+
